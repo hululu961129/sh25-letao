d882299d276f016fbb6f4007b36fee2c214d505d
hululu <1730896401@qq.com>
2020-12-02 15:38:21 +0800

    ajax登录校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -16,6 +16,9 @@
                         min:2,
                         max:6,
                         message:"用户名长度必须在2-6位"
+                    },
+                    callback:{
+                        message:"当前用户不存在"
                     }
 
                 }
@@ -30,10 +33,43 @@
                         min:6,
                         max:12,
                         message:"密码长度必须在6-12位"
+                    },
+                    callback:{
+                        message:"密码错误"
                     }
                 }
 
             }
         }
     })
+        $('#form').on('success.form.bv',function(e){
+            e.preventDefault();
+            //console.log("被阻止了");
+            $.ajax({
+                type:"post",
+                url:"/employee/employeeLogin" ,
+                data:$('#form').serialize(),
+                dataType:"json",
+                success:function(info){
+                 console.log(info)
+                if( info.success ){
+                location.href="index.html";
+              }
+                if(info.error===1000){
+                    //alert("当前用户不存在")
+                    $('#form').data("bootstrapValidator").updateStatus("username","INVALID","callback");
+                }
+                    if(info.error===1001){
+                        //alert("密码错误")
+                    $('#form').data("bootstrapValidator").updateStatus("password","INVALID","callback");
+
+                    }
+
+            }
+
+            })
+        })
+        $('[type="reset"]').click(function () {
+            $('#form').data("bootstrapValidator").resetForm();
+        })
     });
