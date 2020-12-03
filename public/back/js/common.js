60ecab93ecc01d3c436d7cf4b50efe484eec6238
hululu <1730896401@qq.com>
2020-12-03 11:54:08 +0800

    首页-公共功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,4 @@
+//进度条
 $(document).ajaxStart(function () {
     NProgress.start();
 
@@ -8,4 +9,51 @@ $(document).ajaxStop(function(){
 
     },500)
 })
+//登录拦截功能
+//这时候才证明不在登录页面才需要校验 地址栏中没有login。html
+if(location.href.indexOf("login.html")===-1){
+    $.ajax({
+        type:"get",
+        url:"/employee/checkRootLogin",
+        dataType:"json",
+        success:function(info){
+            console.log(info);
+            if(info.success){
 
+            }
+            if(info.error===400){
+                location.href="login.html"
+            }
+        }
+    })
+}
+
+//分类管理的切换功能
+$('.nav .category').click(function(){
+    $('.nav .child').stop().slideToggle();
+})
+//侧边栏切换功能
+$('.icon-menu').click(function () {
+    $('.lt_aside').toggleClass("hidemenu");
+    $('.lt_topbar').toggleClass("hidemenu");
+    $('.lt_main').toggleClass("hidemenu");
+
+})
+//显示模态框
+$('.icon-logout').click(function () {
+    $('#logoutModal').modal('show');
+})
+//退出功能
+$('#logoutBtn').click(function () {
+    $.ajax({
+        type:"get",
+        url:"/employee/employeeLogout",
+        dataType:"json",
+        success:function(info){
+            console.log(info);
+            if(info.success){
+                location.href="login.html"
+            }
+        }
+    })
+})
